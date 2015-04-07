5928e29b6098f049e1b83a9b560101420b7e7079
Shweta <Shweta@lawn-128-61-61-123.lawn.gatech.edu>
2015-04-07 18:54:00 -0400

    Projects error fixed

--- jill_server/jill_server/urls.py
+++ jill_server/jill_server/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
 
 # ProjectsManager API Calls
 	url(r'^api/project/$', ProjectsManager.projectRequest, name='project'),	
-	url(r'^api/project/list/$', ProjectsManager.createProject, name='createProject'),
+	# url(r'^api/project/list/$', ProjectsManager.createProject, name='project'),
 	url(r'^api/project/(?P<project_id>\d*)/$', ProjectsManager.projectRequest, name='projectReturn'),
 
 #ReferebcePaperManager API alls
